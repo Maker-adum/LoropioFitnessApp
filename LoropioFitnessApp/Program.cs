6e8e156717e90fd103ca2c48b8d0b0310ee0af81
Adum Awan <adummaker02@gmail.com>
2023-12-05 17:38:17 +0300

    Second commit

--- LoropioFitnessApp/Program.cs
+++ LoropioFitnessApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Activity Details");
+
+Console.WriteLine("Activity Name");
+string activityName = Console.ReadLine();
+
+Console.WriteLine("Distance Covered (in meters)");
+double distanceInMeters = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Time Taken (in Minutes)");
+double timeMinutes = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Date of Activity (YYY-MM-DD)");
+string dateOfActivity = Console.ReadLine();
+
+double distanceInKilometers = distanceInMeters / 1000;
+double averageSpeed = distanceInKilometers / timeMinutes;
+
+Console.WriteLine();
+Console.WriteLine("Activity Details");
+Console.WriteLine($"Activity Name: {activityName}");
+Console.WriteLine($"Distance Covered: {distanceInKilometers} kilometers");
+Console.WriteLine($"Time Taken: {timeMinutes} minutes");
+Console.WriteLine($"Date of Activity: {dateOfActivity}");
+Console.WriteLine($"Average Speed: {averageSpeed} kilometers per minute");
