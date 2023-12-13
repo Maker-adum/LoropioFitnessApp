ddb8a54fa118c3edb6780de6351b79539b5f5bba
Adum Awan <adummaker02@gmail.com>
2023-12-13 11:35:21 +0300

    Second commit

--- LoropioFitnessApp/Program.cs
+++ LoropioFitnessApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int feeling;
+
+Console.WriteLine("How did you feel after the activity? Enter a number (1=BAD, 2=OK, 3=GOOD, 4=VERY GOOD, 5=STRONG): ");
+string feelingInput = Console.ReadLine();
+
+if (int.TryParse(feelingInput, out feeling))
+{
+    string feelingMessage;
+
+    switch (feeling)
+    {
+        case 1:
+            feelingMessage = "Bad";
+            break;
+        case 2:
+            feelingMessage = "OK";
+            break;
+        case 3:
+            feelingMessage = "Good";
+            break;
+        case 4:
+            feelingMessage = "Very Good";
+            break;
+        case 5:
+            feelingMessage = "Strong";
+            break;
+        default:
+            Console.WriteLine("Invalid feeling input.");
+            return;
+    }
+
+    Console.WriteLine("How You Felt After the Activity: " + feelingMessage);
+}
+else
+{
+    Console.WriteLine("Invalid feeling input. Please enter a valid number.");
+}
