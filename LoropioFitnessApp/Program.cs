92ad13123837a1ac0f458dae48ca3c7486aed832
Adum Awan <adummaker02@gmail.com>
2023-12-13 13:41:40 +0300

    Chapter 4

--- LoropioFitnessApp/Program.cs
+++ LoropioFitnessApp/Program.cs
@@ -1,42 +1,47 @@
 ﻿Console.WriteLine("Please enter your name");
 string lionName = Console.ReadLine();
 
-Console.WriteLine("Hello " + lionName + ", Welcome to Loropio Fitness App. ");
+Console.WriteLine($"Hello {lionName}, Welcome to Loropio Fitness App.");
 
 int feeling;
 
-Console.WriteLine("How did you feel after the activity? Enter a number (1=BAD, 2=OK, 3=GOOD, 4=VERY GOOD, 5=STRONG): ");
-string feelingInput = Console.ReadLine();
+ Console.WriteLine("How did you feel after the activity? Enter a number (1=BAD, 2=OK, 3=GOOD, 4=VERY GOOD, 5=STRONG): ");
+ string feelingInput = Console.ReadLine();
 
-if (int.TryParse(feelingInput, out feeling))
-{
-    string feelingMessage;
+        if (int.TryParse(feelingInput, out feeling))
+        {
+            string feelingMessage;
 
-    switch (feeling)
-    {
-        case 1:
-            feelingMessage = "Bad";
-            break;
-        case 2:
-            feelingMessage = "OK";
-            break;
-        case 3:
-            feelingMessage = "Good";
-            break;
-        case 4:
-            feelingMessage = "Very Good";
-            break;
-        case 5:
-            feelingMessage = "Strong";
-            break;
-        default:
-            Console.WriteLine("Invalid feeling input.");
+            switch (feeling)
+            {
+                case 1:
+                    feelingMessage = "Bad";
+                    break;
+                case 2:
+                    feelingMessage = "OK";
+                    break;
+                case 3:
+                    feelingMessage = "Good";
+                    break;
+                case 4:
+                    feelingMessage = "Very Good";
+                    break;
+                case 5:
+                    feelingMessage = "Strong";
+                    break;
+                default:
+                    Console.WriteLine("Invalid feeling input.");
+                    return;
+            }
+
+            Console.WriteLine($"How You Felt After the Activity: {feelingMessage}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid feeling input. Please enter a valid number.");
             return;
-    }
+        }
 
-    Console.WriteLine("How You Felt After the Activity: " + feelingMessage);
-}
-else
-{
-    Console.WriteLine("Invalid feeling input. Please enter a valid number.");
-}
+   
+    
+
