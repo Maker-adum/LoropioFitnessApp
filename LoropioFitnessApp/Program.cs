6f1e2830987e4fa855d00e70a0a3984b1cb3f752
Adum Awan <adummaker02@gmail.com>
2023-12-13 14:16:38 +0300

    added activity details

--- LoropioFitnessApp/Program.cs
+++ LoropioFitnessApp/Program.cs
@@ -10,10 +10,39 @@ string lionName = Console.ReadLine();
 
 Console.WriteLine($"Hello {lionName}, Welcome to Loropio Fitness App.");
 
+Console.WriteLine("Activity Details");
+
+Console.WriteLine("Activity Name");
+string activityName = Console.ReadLine();
+
+Console.WriteLine("Distance Covered (in meters)");
+double distanceInMeters = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Time Taken (in Minutes)");
+double timeMinutes = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Date of Activity (YYY-MM-DD)");
+string dateOfActivity = Console.ReadLine();
+
+double distanceInKilometers = distanceInMeters / 1000;
+double averageSpeed = distanceInKilometers / timeMinutes;
+
+Console.WriteLine();
+Console.WriteLine("Activity Details");
+Console.WriteLine($"Activity Name: {activityName}");
+Console.WriteLine($"Distance Covered: {distanceInKilometers} kilometers");
+Console.WriteLine($"Time Taken: {timeMinutes} minutes");
+Console.WriteLine($"Date of Activity: {dateOfActivity}");
+Console.WriteLine($"Average Speed: {averageSpeed} kilometers per minute");
+Console.WriteLine($"You feel{feelingInput}");
+
+Console.ReadLine();
+
 int feeling;
 
  Console.WriteLine("How did you feel after the activity? Enter a number (1=BAD, 2=OK, 3=GOOD, 4=VERY GOOD, 5=STRONG): ");
  string feelingInput = Console.ReadLine();
+
 
         if (int.TryParse(feelingInput, out feeling))
         {
@@ -49,6 +78,6 @@ int feeling;
             return;
         }
 
-   
-    
+
+
 
